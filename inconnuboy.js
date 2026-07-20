af3a3dc330d346c2a3d7678feeea9dfedaaeca3f
TEDDY TECH <yea9361@gmail.com>
2026-07-20 22:29:15 +0300

    Enhance cmd function with react and event type
    Added support for reaction and event type in cmd function.

--- inconnuboy.js
+++ inconnuboy.js
@@ -4,14 +4,17 @@ function cmd(info, func) {
     var data = info;
     data.function = func;
     
-    // Si pas de pattern, on utilise cmdname
+    // Auto set pattern from cmdname if missing
     if (!data.pattern && data.cmdname) data.pattern = data.cmdname;
     
+    // Defaults
     if (!data.alias) data.alias = [];
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
     if (!data.desc) data.desc = '';
     if (!data.fromMe) data.fromMe = false;
     if (!data.category) data.category = 'misc';
+    if (!data.react) data.react = ''; // FIX 1: add react support
+    if (!data.on) data.on = 'body'; // FIX 2: for body/text/image/sticker events
     
     commands.push(data);
     return data;
@@ -23,4 +26,3 @@ module.exports = {
     Function: cmd,
     commands,
 };
-
